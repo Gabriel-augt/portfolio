d36b4a10fe7c6baf24fa61785e691c48af3f91cb
Gabriel Augusto <117189018+Gabriel-augt@users.noreply.github.com>
2024-08-30 22:50:43 -0400

    fix path

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <script setup>
 import { RouterLink, RouterView } from 'vue-router'
 import HomeNavBar from './components/HomeNavBar.vue';
-import ProjectsNavBar from './ProjectsNavBar.vue';
+import ProjectsNavBar from './components/ProjectsNavBar.vue';
 import AboutMeNavBar from './components/AboutMeNavBar.vue';
 import { ref } from 'vue';
 
