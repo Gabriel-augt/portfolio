3e1de2de53465f49144214bc621df0f9b0133a4c
Gabriel Augusto <117189018+Gabriel-augt@users.noreply.github.com>
2024-09-03 18:17:21 -0400

    horizontal bars correction

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -11,8 +11,8 @@ import HomeButtons from './HomeButtons.vue';
     </h1>
 
     <h2 class="text-white-75 text-3xl font-manrope tracking-wider font-light z-10
-    after:bg-whitext-white-75 after:w-[1px] after:h-20 after:absolute after:rotate-90 after:ml-16 after:-translate-y-5
-    before:bg-whitext-white-75 before:w-[1px] before:h-20 before:absolute before:rotate-90 before:-ml-16 before:-translate-y-5
+    after:bg-white-75 after:w-[1px] after:h-20 after:absolute after:rotate-90 after:ml-16 after:-translate-y-5
+    before:bg-white-75 before:w-[1px] before:h-20 before:absolute before:rotate-90 before:-ml-16 before:-translate-y-5
     max-sm:after:h-6 max-sm:after:ml-7 max-sm:after:translate-y-[5px] max-sm:before:h-6 max-sm:before:-ml-7
     max-sm:before:translate-y-[5px] max-sm:text-2xl max-sm:overflow-hidden max-[375px]:after:ml-4 max-[375px]:after:h-4
     max-[375px]:after:translate-y-[10px] max-[375px]:before:h-4 max-[375px]:before:-ml-4 max-[375px]:before:translate-y-[10px]
